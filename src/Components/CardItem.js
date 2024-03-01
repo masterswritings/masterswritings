ce37a63c369ca0b075f5cac2e65e8be08b06dae2
srinivaschellaboina <123400056+srinivaschellaboina@users.noreply.github.com>
2024-02-29 20:35:24 -0500

    o commit

--- src/Components/CardItem.js
+++ src/Components/CardItem.js
@@ -2,17 +2,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function CardItem(props) {
-  const { src, texts, label, path } = props; // Assuming texts is an array
+function CardItem({ src, texts, label, path }) {
+  // Ensure src uses process.env.PUBLIC_URL for correct path resolution
+  const imagePath = `${process.env.PUBLIC_URL}/${src}`;
 
   return (
     <li className='cards__item'>
       <Link className='cards__item__link' to={path}>
         <figure className='cards__item__pic-wrap' data-category={label}>
-          <img className='cards__item__img' alt='Img' src={src} />
+          <img className='cards__item__img' alt='Img' src={imagePath} />
         </figure>
         <div className='cards__item__info'>
-          {texts.map((text, index) => ( // Render each text as a paragraph or heading
+          {texts.map((text, index) => (
             <p key={index} className='cards__item__text'>{text}</p>
           ))}
         </div>
